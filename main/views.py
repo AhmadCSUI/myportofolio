ba634814d8a85f66f79cae37c0e3894b105def20
AhmadCSUI <ahmad.shanahan@ui.ac.id>
2026-09-09 15:05:24 +0700

    feat(docs): mulai nyoba bikin docs buat page baru

--- main/views.py
+++ main/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from main.models import Experience
 
 # Create your views here.
+full_name = "Ahmad S. Zorya"
+name = "Ahmad"
+npm = "2506541894"
+likes = {"cats", "kittens", "substantive discussion"}
+dislikes = {"benjamin netanyahu", "idle talks", "insincerity"}
 
 def show_main(request):
     context = {
-        "name": "Ahmad",
-        "npm": "2506541894",
+        "name": name,
+        "npm": npm,
         "study_program": "S1 Ilmu Komputer",
         "bio": (
             "Sukatidur"
@@ -22,3 +27,8 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_docs(request):
+    context = {
+            }
+    return render(request, "docs.html", context)
