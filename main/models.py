7bedc5bdf856170ee1da1614dfc7ed6b1c39bf9f
AhmadCSUI <ahmad.shanahan@ui.ac.id>
2026-09-16 12:59:55 +0700

    benerin model edu dan exp di started_at biar bisa diisi

--- main/models.py
+++ main/models.py
@@ -16,7 +16,7 @@ class Experience(models.Model):
     description = models.TextField()
     category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
     thumbnail = models.URLField(blank=True, null=True)
-    started_at = models.DateTimeField(auto_now_add=True)
+    started_at = models.DateTimeField()
     ended_at = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.title
@@ -35,7 +35,7 @@ class Education(models.Model):
     title = models.CharField(max_length=255)
     score = models.FloatField(default=0.00)
     category = models.CharField(max_length=20, choices=EDUCATION_CHOICES, default='formal')
-    started_at = models.DateTimeField(auto_now_add=True)
+    started_at = models.DateTimeField()
     ended_at = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.title
