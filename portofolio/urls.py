04e02455574c9757ae07b87c27d686fd08c5047c
AhmadCSUI <ahmad.shanahan@ui.ac.id>
2026-09-16 12:43:18 +0700

    shrink index.html

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
+    path("education/add/", create_education, name="create_education"),
 ]
